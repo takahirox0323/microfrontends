9dd991c7834d876d178a886aa0b885936a468ea8
takahirox0323 <takahiro.arai0323@gmail.com>
2025-02-21 02:45:28 +0900

    test

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,14 +3,12 @@
 const nextConfig = {
   basePath: "/docs",
   assetPrefix: "/docs-static",
- beforeFiles: [
-        // This rewrite is necessary to support assetPrefix only in Next 14 and below.
-        // It is not necessary in Next 15.
-        {
-          source: '/docs-static/_next/:path*',
-          destination: '/_next/:path*',
-        },
-      ],
+  async rewrites() {
+    return [ {
+      source: '/docs-static/_next/:path*',
+      destination: '/_next/:path*',
+    },]
+}
 }
 
 module.exports = nextConfig
