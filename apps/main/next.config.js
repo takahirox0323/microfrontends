476426854e047006de44ec6935888378e52e6f25
takahirox0323 <takahiro.arai0323@gmail.com>
2025-02-21 03:15:49 +0900

    test

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -6,20 +6,20 @@ const nextConfig = {
        * Rewrites for Multi-Zones
        */
       {
-        source: '/docs',
-        destination: `${process.env.DOCS_URL}/docs`,
-      },
-      {
-        source: '/docs/:path*',
-        destination: `${process.env.DOCS_URL}/docs/:path*`,
+        source: '/docs/_next/:path*',
+        destination: `${process.env.DOCS_URL}/docs/_next/:path*`,
       },
       {
         source: '/docs-static/:path*',
         destination: `${process.env.DOCS_URL}/docs/:path*`,
       },
       {
-        source: '/docs/_next/:path*',
-        destination: `${process.env.DOCS_URL}/docs/_next/:path*`,
+        source: '/docs/:path*',
+        destination: `${process.env.DOCS_URL}/docs/:path*`,
+      },
+      {
+        source: '/docs',
+        destination: `${process.env.DOCS_URL}/docs`,
       },
     ]
   },
