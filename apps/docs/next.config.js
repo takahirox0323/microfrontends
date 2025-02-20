ff76ef47e60125dfe436cdfe953bde75de03aea0
takahirox0323 <takahiro.arai0323@gmail.com>
2025-02-21 02:21:28 +0900

    test

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,19 +1,8 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: 'https://develop-docs.d1n4gq1r6iigwu.amplifyapp.com',
   basePath: "/docs",
-  async rewrites() {
-    return [
-      /**
-       * Rewrites for Multi-Zones
-       */
-      {
-        source: '/docs/_next/:path*',
-        destination: 'https://develop-docs.d1n4gq1r6iigwu.amplifyapp.com/_next/:path*',
-      }
-    ]
-  },
+
 }
 
 module.exports = nextConfig
