ff1dcab487e91ec7b0737ac698ecc57015d24868
takahirox0323 <takahiro.arai0323@gmail.com>
2025-02-21 03:36:42 +0900

    test

--- packages/acme-components/src/navbar.tsx
+++ packages/acme-components/src/navbar.tsx
@@ -18,11 +18,13 @@ export function Navbar({ isDocsApp }: { isDocsApp?: boolean }) {
   ) : (
     <ul className="inline-flex mb-4">
       <li>
-        <a href="/">Docs (Multi-Zones)</a>
+        <a href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/">
+          Docs (Multi-Zones)
+        </a>
       </li>
-      <li className="ml-4">
-        <a href="/about">Docs (Multi-Zones)</a>
-      </li>
+      <a href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/about">
+        Docs (Multi-Zones)
+      </a>
       <li className="ml-4">
         <a href="/docs">Docs (Multi-Zones)</a>
       </li>
