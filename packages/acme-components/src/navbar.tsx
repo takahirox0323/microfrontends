cff7ceb85dfe408eaea9681eb3b84f3c4ad8422c
takahirox0323 <takahiro.arai0323@gmail.com>
2025-02-21 15:24:34 +0900

    test

--- packages/acme-components/src/navbar.tsx
+++ packages/acme-components/src/navbar.tsx
@@ -4,29 +4,43 @@ export function Navbar({ isDocsApp }: { isDocsApp?: boolean }) {
   return isDocsApp ? (
     <ul className="inline-flex mb-4">
       <li>
-        <a href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/">
-          Docs (Multi-Zones)
+        <a
+          style={{ color: 'blue' }}
+          href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/"
+        >
+          メインアプリ
         </a>
       </li>
-      <a href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/about">
-        Docs (Multi-Zones)
+      <a
+        style={{ color: 'blue' }}
+        href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/about"
+      >
+        メインアプリ
       </a>
       <li className="ml-4">
-        <a href="/docs">Docs (Multi-Zones)</a>
+        <a style={{ color: 'red' }} href="/docs">
+          サブアプリ
+        </a>
       </li>
     </ul>
   ) : (
     <ul className="inline-flex mb-4">
       <li>
-        <a href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/">
-          Docs (Multi-Zones)
+        <a
+          style={{ color: 'blue' }}
+          href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/"
+        >
+          メインアプリ
         </a>
       </li>
-      <a href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/about">
-        Docs (Multi-Zones)
+      <a
+        style={{ color: 'blue' }}
+        href="https://develop-main.d3ebifrucnlqj9.amplifyapp.com/about"
+      >
+        メインアプリ
       </a>
-      <li className="ml-4">
-        <a href="/docs">Docs (Multi-Zones)</a>
+      <li style={{ color: 'red' }} className="ml-4">
+        <a href="/docs">サブアプリ</a>
       </li>
     </ul>
   )
